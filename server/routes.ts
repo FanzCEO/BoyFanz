31e0f76a7820083468ce7dfb5ed61049a5f01bf3
Joshua Stone <jstonecna@gmail.com>
2026-01-08 19:29:39 -0500

    Mount mediaHub routes under /api/mediahub

--- server/routes.ts
+++ server/routes.ts
@@ -24,6 +24,7 @@ import safetyRoutes from './routes/safetyRoutes';
 import aiRoutes from './routes/aiRoutes';
 import aiGatewayRoutes from './routes/aiGatewayRoutes';
 import liveChatRoutes from './routes/liveChatRoutes';
+import mediaHubRoutes from './routes/mediaHubRoutes';
 // Admin Management Routes
 import brandingRoutes from './routes/admin/branding';
 import bookingsRoutes from './routes/admin/bookings';
